98bdcce73e5e6c5f1fd78f4a33a41b4206a78619
webermg <matthew.gerald.weber@gmail.com>
2020-10-05 15:14:46 -0700

    added questions

--- questions.js
+++ questions.js
@@ -6,6 +6,40 @@ const questions = [
     choices: ["Engineer", "Manager", "Intern"]
   },
   {
+    type: 'input',
+    name: "name",
+    message: "Name?"
+  },
+  {
+    type: 'input',
+    name: "id",
+    message: "ID?"
+  },
+  {
+    type: 'input',
+    name: "email",
+    message: "Email?"
+  },
+  {
+    type: 'input',
+    name: "officeNum",
+    message: "Office Number?",
+    when: answers => answers.role === "Manager"
+  },
+  {
+    type: 'input',
+    name: "github",
+    message: "GitHub?",
+    when: answers => answers.role === "Engineer"
+  },
+  {
+    type: 'input',
+    name: "school",
+    message: "School?",
+    when: answers => answers.role === "Intern"
+  },
+ 
+  {
     type: "confirm",
     name: 'askAgain',
     message: "Add another?"
